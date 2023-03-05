c6042eebafa76809040067ee6199c51f15d34fb4
bud42 <bdboyd42@gmail.com>
2023-03-04 19:51:27 -0500

    now with edat audits

--- src/issues/__init__.py
+++ src/issues/__init__.py
@@ -1,27 +1,260 @@
-"""
+"""Garjus Issues Management."""
+import logging
+import importlib
 
-Garjus Issues Management
 
-"""
+def update(garjus, projects=None):
+    """Update issues."""
+    for p in (projects or garjus.projects()):
+        if p in projects:
+            logging.info(f'updating issues:{p}')
+            update_project(garjus, p)
 
-def delete_old_issues(garjus):
-	pass
 
-def update(garjus):
-	pass
+def update_project(garjus, project):
+    """Update project issues."""
+    results = []
 
-def matching_issues(issue1, issue2):
-	# Matching means both issues are of the same Type
+    # Load the project primary redcap
+    project_redcap = garjus.primary(project)
+    if not project_redcap:
+        logging.debug('primary redcap not found, cannot update issues')
+        return
+
+    # Confirm project exists on XNAT
+    if not garjus.project_exists(project):
+        msg = f'destination project does not exist:{project}'
+        logging.debug(msg)
+        return
+
+    # Audit edats
+    for p in garjus.edat_protocols(project):
+        # Load event list from comma-delimited value
+        events = p['edat_events']
+        events = [x.strip() for x in events.split(',')]
+
+        # Run the audit
+        results += _audit_edat(
+            project_redcap,
+            events,
+            p['edat_rawfield'],
+            p['edat_convfield'],
+            p['edat_readyfield'])
+
+    # Scanning protocols
+    for p in garjus.scanning_protocols(project):
+        events = p['scanning_events']
+        date_field = p['scanning_datefield']
+        sess_field = p['scanning_srcsessfield']
+        sess_suffix = p['scanning_xnatsuffix']
+        src_project = p['scanning_srcproject']
+
+        src_labels = garjus.session_labels(src_project)
+        dst_labels = garjus.session_labels(project)
+
+        # Get events list
+        events = None
+        if p.get('scanning_events', False):
+            events = [x.strip() for x in p['scanning_events'].split(',')]
+
+        # check that projects exist on XNAT
+        if not garjus.source_project_exists(src_project):
+            msg = f'source project does not exist in XNAT:{src_project}'
+            logging.error(msg)
+            garjus.add_issue(msg, category='ERROR', project=project)
+            continue
+
+        # Check for alternate redcap
+        if p['scanning_altprimary'] != '':
+            _rc = garjus.alternate(p['scanning_altprimary'])
+        else:
+            _rc = project_redcap
+
+        # Make the scan table linking scan ID to subject/session
+        scan_table = _make_scan_table(
+            _rc,
+            events,
+            date_field,
+            sess_field,
+            sess_suffix)
+
+        results += _audit_scanning(scan_table, src_labels, dst_labels)
+
+    _add_issues(garjus, results, project=project)
+
+
+def _matching_issues(issue1, issue2):
+    # Matching means both issues are of the same category
     # on the same Project/Subject
     # and as applicable, the same XNAT Session/Scan
     # and as applicable the same REDCap Event/Field
     keys = [
-        'main_name', 'issue_type', 'issue_subject',
-        'issue_session', 'issue_scan', 'issue_event', 'issue_field']
+        'PROJECT', 'CATEGORY', 'SUBJECT', 'SESSION', 'SCAN', 'EVENT', 'FIELD']
 
     for k in keys:
-        if (k in issue1) and (issue1[k] != issue2[k]):
+        if (k.lower() in issue1) and (issue1[k.lower()] != issue2[k]):
             return False
 
     return True
 
+
+def _audit_edat(project, events, rawfield, convfield, readyfield):
+    try:
+        audit_edat = importlib.import_module(f'src.issues.audit_edat')
+    except ModuleNotFoundError as err:
+        logging.error(f'error loading:{err}')
+        return
+
+    return audit_edat.audit(project, events, rawfield, convfield, readyfield)
+
+
+def _audit_scanning(scan_table, src_project, project):
+    try:
+        audit_imaging = importlib.import_module(f'src.issues.audit_imaging')
+    except ModuleNotFoundError as err:
+        logging.error(f'error loading:{err}')
+        return
+
+    return audit_imaging.audit(scan_table, src_project, project)
+
+
+def _add_issues(garjus, records, project=None):
+    new_issues = []
+    old_issues = []
+    has_errors = False
+  
+    # First check existing issues,
+    # import new issues and update existing,
+    # complete(or delete???) any no longer found
+
+    # Check for errors
+    for r in records:
+        if r['category'] == 'ERROR':
+            has_errors = True
+            break
+
+    # Set project
+    if project:
+        for r in records:
+            r['project'] = project
+
+    # Compare to current issues
+    cur_issues = garjus.issues(project)
+    new_issues = _find_new(cur_issues, records)
+
+    # Upload new records
+    if new_issues:
+        logging.info(f'uploading {len(new_issues)} new issues')
+        garjus.add_issues(new_issues)
+    else:
+        logging.info('no new issues to upload')
+
+    if has_errors:
+        logging.info(f'errors during audit, not closing old issues')
+        return
+
+    # Close fixed issues
+    fixed_issues = _find_fixed(cur_issues, records)
+    if fixed_issues:
+        logging.info(f'setting {len(fixed_issues)} resolved issues to complete')
+        garjus.close_issues(fixed_issues)
+        # Delete old resolved issues
+        logging.info(f'TBD:delete_old_issues')
+    else:
+        logging.info('no resolved issues to complete')
+
+
+def _find_new(issues, records):
+    results = []
+
+    for rec in records:
+        isnew = True
+
+        # Try to find a matching record
+        for cur in issues.to_dict('records'):
+            cur_id = cur['ID']
+            cur_proj = cur['PROJECT']
+            if _matching_issues(rec, cur):
+                isnew = False
+                logging.debug(f'matches existing issue:{cur_proj}:{cur_id}')
+                break
+
+        if isnew:
+            results.append(rec)
+    return results
+
+
+def _find_fixed(issues, records):
+    """Return issues that are not in current search, i.e. resolved"""
+    results = []
+    # Find old issues
+    logging.debug('checking for resolved issues')
+    for cur in issues.to_dict('records'):
+        isold = True
+        cur_id = cur['ID']
+        cur_proj = cur['PROJECT']
+
+        # Try to find a matching record
+        for rec in records:
+            if _matching_issues(rec, cur):
+                isold = False
+                logging.debug(f'matches existing issue:{cur_proj}:{cur_id}')
+                break
+
+        if isold:
+            # Append to list as closed with current time
+            logging.debug(f'found resolved issue:{cur_proj}:{cur_id}')
+            results.append({'project': cur_proj, 'id': cur_id})
+
+    return results
+
+
+def _make_scan_table(
+    project,
+    events,
+    date_field,
+    sess_field,
+    scan_suffix):
+    """Make the scan table, linking source to destination subject/session."""
+    data = []
+    id2subj = {}
+
+    # Shortcut
+    def_field = project.def_field
+
+    # Handle secondary ID
+    sec_field = project.export_project_info()['secondary_unique_field']
+    if sec_field:
+        rec = project.export_records(fields=[def_field, sec_field])
+        id2subj = {x[def_field]: x[sec_field] for x in rec if x[sec_field]}
+
+    # Get mri records from redcap
+    fields = [date_field, sess_field, def_field]
+    try:
+        rec = project.export_records(fields=fields, events=events)
+    except Exception as err:
+        logging.error(err)
+        return []
+
+    # Only if date is entered
+    rec = [x for x in rec if x[date_field]]
+
+    # Only if entered
+    rec = [x for x in rec if x[sess_field]]
+
+    # Set the subject and session
+    for r in rec:
+        d = {}
+        d['scandate'] = r[date_field]
+        d['src_session'] = r[sess_field]
+        d['src_subject'] = d['src_session']
+        d['dst_subject'] = id2subj.get(r[def_field], r[def_field])
+        d['dst_session'] = d['dst_subject'] + scan_suffix
+        if 'redcap_event_name' in r:
+            d['event'] = r['redcap_event_name']
+        else:
+            d['event'] = ''
+
+        data.append(d)
+
+    return data
