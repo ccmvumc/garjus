27a98f52af2c2d8e164d5b673878ae23098f7765
bud42 <bdboyd42@gmail.com>
2023-03-10 11:28:32 -0500

    examiner now working

--- src/automations/__init__.py
+++ src/automations/__init__.py
@@ -21,8 +21,6 @@ def update(garjus, projects, autos_include=None, autos_exclude=None):
 
 def update_project(garjus, project, autos_include=None, autos_exclude=None):
     """Update automations for project."""
-
-    # Get filtered list for this project
     scan_autos = garjus.scan_automations(project)
 
     if autos_include:
@@ -95,13 +93,13 @@ def _run_etl_automation(automation, garjus, project):
 
 
 def _run_etl_nihexaminer(project):
-    """Process examiner files from REDCap and upload results"""
+    """Process examiner files from REDCap and upload results."""
     data = {}
     results = []
     events = []
     fields = []
     records = []
-    flanker_field = 'flanker_file'
+    flank_field = 'flanker_file'
     nback_field = 'nback_upload'
     shift_field = 'set_shifting_file'
     cpt_field = 'cpt_upload'
@@ -114,8 +112,54 @@ def _run_etl_nihexaminer(project):
         logging.error(f'error loading module:examiner:{err}')
         return
 
+    if project.export_project_info()['project_title'] == 'Depressed Mind 2':
+        # Alternate file field names
+        flank_field = 'flanker_summfile'
+        nback_field = 'nback_summfile'
+        shift_field = 'set_shifting_summfile'
+        cpt_field = 'cpt_summfile'
+
     # Get the fields
-    fields = [project.def_field, done_field, cpt_field]
+    fields = [
+        project.def_field,
+        done_field,
+        cpt_field,
+        'dot_count_tot',
+        'anti_trial_1',
+        'anti_trial_2',
+        'correct_f',
+        'correct_l',
+        'correct_animal',
+        'correct_veg',
+        'repetition_f',
+        'rule_vio_f',
+        'repetition_l',
+        'rule_vio_l',
+        'repetition_animal',
+        'rule_vio_animal',
+        'repetition_veg',
+        'rule_vio_veg',
+        'brs_1',
+        'brs_2',
+        'brs_3',
+        'brs_4',
+        'brs_5',
+        'brs_6',
+        'brs_7',
+        'brs_8',
+        'brs_9',
+    ]
+
+    if 'D3' in project.export_project_info()['project_title']:
+        fields = fields.extend([
+            'correct_s', 'rule_vio_s, repetition_s',
+            'correct_t', 'rule_vio_t, repetition_t',
+            'correct_fruit', 'rule_vio_fruit, repetition_fruit',
+            'correct_r', 'rule_vio_r, repetition_r',
+            'correct_m', 'rule_vio_m, repetition_m',
+            'correct_cloth', 'rule_vio_cloth, repetition_cloth',
+        ])
+    print(fields)
 
     # Determine events
     events = field2events(project, cpt_field)
@@ -128,13 +172,6 @@ def _run_etl_nihexaminer(project):
         record_id = r[project.def_field]
         event_id = r['redcap_event_name']
 
-
-
-        if record_id != '1':
-            continue
-
-
-
         if r[done_field]:
             logging.debug(f'already ETL:{record_id}:{event_id}')
             continue
@@ -144,27 +181,97 @@ def _run_etl_nihexaminer(project):
             continue
 
         logging.info(f'running nihexaminer ETL:{record_id}:{event_id}')
+
+        # Get values needed for scoring
+        manual_values = {
+            'dot_total': int(r['dot_count_tot']),
+            'anti_trial_1': int(r['anti_trial_1']),
+            'anti_trial_2': int(r['anti_trial_2']),
+            'cf1_corr': int(r['correct_animal']),
+            'cf1_rep': int(r['repetition_animal']),
+            'cf1_rv': int(r['rule_vio_animal']),
+            'brs_1': int(r['brs_1']),
+            'brs_2': int(r['brs_2']),
+            'brs_3': int(r['brs_3']),
+            'brs_4': int(r['brs_4']),
+            'brs_5': int(r['brs_5']),
+            'brs_6': int(r['brs_6']),
+            'brs_7': int(r['brs_7']),
+            'brs_8': int(r['brs_8']),
+            'brs_9': int(r['brs_9']),
+        }
+
+        if r['correct_f']:
+            # examiner version 0
+            manual_values.update({
+                'vf1_corr': int(r['correct_f']),
+                'vf1_rep': int(r['repetition_f']),
+                'vf1_rv': int(r['rule_vio_f']),
+                'vf2_corr': int(r['correct_l']),
+                'vf2_rep': int(r['repetition_l']),
+                'vf2_rv': int(r['rule_vio_l']),
+                'cf2_corr': int(r['correct_veg']),
+                'cf2_rep': int(r['repetition_veg']),
+                'cf2_rv': int(r['rule_vio_veg'])
+            })
+        elif r['correct_t']:
+            # examiner version 1
+            manual_values.update({
+                'vf1_corr': int(r['correct_t']),
+                'vf1_rep': int(r['repetition_t']),
+                'vf1_rv': int(r['rule_vio_t']),
+                'vf2_corr': int(r['correct_s']),
+                'vf2_rep': int(r['repetition_s']),
+                'vf2_rv': int(r['rule_vio_s']),
+                'cf2_corr': int(r['correct_fruit']),
+                'cf2_rep': int(r['repetition_fruit']),
+                'cf2_rv': int(r['rule_vio_fruit'])
+            })
+        else:
+            # examiner version 2
+            manual_values.update({
+                'vf1_corr': int(r['correct_r']),
+                'vf1_rep': int(r['repetition_r']),
+                'vf1_rv': int(r['rule_vio_r']),
+                'vf2_corr': int(r['correct_m']),
+                'vf2_rep': int(r['repetition_m']),
+                'vf2_rv': int(r['rule_vio_m']),
+                'cf2_corr': int(r['correct_cloth']),
+                'cf2_rep': int(r['repetition_cloth']),
+                'cf2_rv': int(r['rule_vio_cloth'])
+            })
+
         with tempfile.TemporaryDirectory() as tmpdir:
-            flanker_file = f'{tmpdir}/flanker.csv'
+            # Get files needed
+            flank_file = f'{tmpdir}/flanker.csv'
             cpt_file = f'{tmpdir}/cpt.csv'
-            nback_file =f'{tmpdir}/nback.csv'
-            shift_file =f'{tmpdir}/shift.csv'
+            nback_file = f'{tmpdir}/nback.csv'
+            shift_file = f'{tmpdir}/shift.csv'
 
-            # Download files from redcap
-            logging.debug(f'download files:{record_id}:{event_id}:{flanker_file}')
-            download_file(project, record_id, event_id, flanker_field, flanker_file)
-            logging.debug(f'download NBack:{record_id}:{event_id}:{nback_field}')
-            download_file(project, record_id, event_id, nback_field, nback_file)
-            logging.debug(f'download Shift:{record_id}:{event_id}:{shift_field}')
-            download_file(project, record_id, event_id, shift_field, shift_file)
-            logging.debug(f'download CPT:{record_id}:{event_id}:{cpt_field}')
-            download_file(project, record_id, event_id, cpt_field, cpt_file)
-
-            # Extract data from file
             try:
-                data = examiner.process(flanker_file, cpt_file, nback_file, shift_file)
+                # Download files from redcap
+                logging.debug(f'download files:{record_id}:{event_id}:{flank_file}')
+                download_file(project, record_id, event_id, flank_field, flank_file)
+                logging.debug(f'download NBack:{record_id}:{event_id}:{nback_field}')
+                download_file(project, record_id, event_id, nback_field, nback_file)
+                logging.debug(f'download Shift:{record_id}:{event_id}:{shift_field}')
+                download_file(project, record_id, event_id, shift_field, shift_file)
+                logging.debug(f'download CPT:{record_id}:{event_id}:{cpt_field}')
+                download_file(project, record_id, event_id, cpt_field, cpt_file)
             except Exception as err:
                 logging.error(f'downloading files:{record_id}:{event_id}')
+                continue
+
+            try:
+                # Process inputs
+                data = examiner.process(
+                    manual_values,
+                    flank_file,
+                    cpt_file,
+                    nback_file,
+                    shift_file)
+            except Exception as err:
+                logging.error(f'processing examiner:{record_id}:{event_id}')
                 continue
 
         # Load data back to redcap
@@ -317,6 +424,7 @@ def _session_relabels(scan_data, site_data):
 def _load(project, record_id, event_id, data):
     data[project.def_field] = record_id
     data['redcap_event_name'] = event_id
+    data = {k: str(v) for k,v in data.items()}
 
     try:
         response = project.import_records([data])
