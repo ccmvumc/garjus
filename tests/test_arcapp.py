b14792cac625ae8b4a3c43fa107392c2c065d626
bud42 <bdboyd42@gmail.com>
2023-07-28 17:05:19 -0400

    Create test_arcapp.py

---
+++ tests/test_arcapp.py
@@ -0,0 +1,19 @@
+import logging
+
+
+if __name__ == "__main__":
+    import redcap
+    from garjus.automations.etl_arcapp import process_project
+
+    api_url = 'https://redcap.vanderbilt.edu/api/'
+    api_key = '70E813FDB14AC969E244CFBD87ED3537'  # this is a test copy of REM
+    rc = redcap.Project(api_url, api_key)
+
+    logging.basicConfig(
+        format='%(asctime)s - %(levelname)s:%(module)s:%(message)s',
+        level=logging.DEBUG,
+        datefmt='%Y-%m-%d %H:%M:%S')
+
+    results = process_project(rc)
+    print(results)
+    print('Done!')
