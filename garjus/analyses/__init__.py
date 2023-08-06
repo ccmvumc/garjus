c5ce2513c7890b8a1080af712320639eaf5c3d82
bud42 <bdboyd42@gmail.com>
2023-08-06 13:59:18 -0400

    download analysis inputs

--- garjus/analyses/__init__.py
+++ garjus/analyses/__init__.py
@@ -2,6 +2,27 @@
 import logging
 import os
 
+import pandas as pd
+
+
+#'inputs': {
+#    'xnat': {
+#       'subjects': {
+#           'sessions': {
+#               'types': 'Baseline',
+#               'assessors': [
+#                   {'name': 'assr_msit', 'types': 'fmri_msit_v3',
+#                       'resources': [
+#                           {'resource': '1stLEVEL', 'fmatch': 'con_0002.nii.gz'},
+#                           {'resource': '1stLEVEL', 'fmatch': 'behavior.txt'}
+# ]}]}}}}]
+
+# OUTPUTS:
+# subject / session / assessor /resource / file
+
+
+logger = logging.getLogger('garjus.analyses')
+
 
 def update(garjus, projects=None):
     """Update analyses."""
@@ -33,60 +54,212 @@ def _update_project(garjus, project):
 def update_analysis(
     garjus,
     project,
-    analysis_id):
-
+    analysis_id
+):
+    """Update analysis."""
     print('TBD:update_analysis')
 
 
-def download_analysis_inputs(garjus, project, analysis_id, download_dir):
-    print(f'download_analysis_inputs:{project}:{analysis_id}:{download_dir}')
+def _sessions_from_scans(scans):
+    return scans[[
+        'PROJECT',
+        'SUBJECT',
+        'SESSION',
+        'SESSTYPE',
+        'DATE',
+        'SITE'
+    ]].drop_duplicates()
 
-    # Make the output directory
+
+def _sessions_from_assessors(assessors):
+    return assessors[[
+        'PROJECT',
+        'SUBJECT',
+        'SESSION',
+        'SESSTYPE',
+        'DATE',
+        'SITE'
+    ]].drop_duplicates()
+
+
+def _download_file(garjus, proj, subj, sess, assr, res, fmatch, dst):
+    # Make the folders for this file path
+    _make_dirs(os.path.dirname(dst))
+
+    # Connect to the resource on xnat
+    res = garjus.xnat().select_assessor_resource(proj, subj, sess, assr, res)
+
+    # TODO: apply regex or wildcards in fmatch
+    #res_obj.files()[0].label()).get(fpath)
+    # res.files().label()
+
+    res.file(fmatch).get(dst)
+    return dst
+
+def _download_resource(garjus, proj, subj, sess, assr, res, dst):
+    # Make the folders for destination path
+    _make_dirs(dst)
+
+    # Connect to the resource on xnat
+    res = garjus.xnat().select_assessor_resource(proj, subj, sess, assr, res)
+
+    # Download extracted
+    res.get(dst, extract=True)
+
+    return dst
+
+
+def _make_dirs(dirname):
     try:
-        os.makedirs(download_dir)
+        os.makedirs(dirname)
     except FileExistsError:
         pass
 
+
+def _download_subject(garjus, subj_dir, subj_spec, proj, subj, sessions, assessors):
+
+    # TODO: handle any subject-level assessors, sgp
+
+    # Download the subjects sessions
+    sess_spec = subj_spec['sessions']
+    sess_types = sess_spec['types'].split(',')
+
+    for i, s in sessions[sessions.SUBJECT == subj].iterrows():
+        sess = s.SESSION
+
+        # Apply session type filter
+        if s.SESSTYPE not in sess_types:
+            continue
+
+        sess_dir = f'{subj_dir}/{sess}'
+        _download_session(
+            garjus, sess_dir, sess_spec, proj, subj, sess, assessors)
+
+
+def _download_session(garjus, sess_dir, sess_spec, proj, subj, sess, assessors):
+    # get the assessors for this session
+    sess_assessors = assessors[assessors.SESSION == sess]
+
+    for k, a in sess_assessors.iterrows():
+        assr = a.ASSR
+
+        for assr_spec in sess_spec['assessors']:
+
+            assr_types = assr_spec['types'].split(',')
+            if a.PROCTYPE not in assr_types:
+                continue
+
+            for res_spec in assr_spec['resources']:
+
+                try:
+                    res = res_spec['resource']
+                except (KeyError, ValueError) as err:
+                    logger.error(f'reading resource:{err}')
+                    continue
+
+                if 'fmatch' in res_spec:
+                    # Download files
+                    for fmatch in res_spec['fmatch'].split(','):
+
+                        # Where shall we save it?
+                        dst = f'{sess_dir}/{assr}/{res}/{fmatch}'
+
+                        # Have we already downloaded it?
+                        if os.path.exists(dst):
+                            logger.debug(f'exists:{dst}')
+                            continue
+
+                        # Download it
+                        logger.debug(f'download file:{proj}:{subj}:{sess}:{assr}:{res}:{fmatch}')
+                        try:
+                            _download_file(
+                                garjus,
+                                proj,
+                                subj,
+                                sess,
+                                assr,
+                                res,
+                                fmatch,
+                                dst
+                            )
+                        except Exception as err:
+                            logger.error(f'{subj}:{sess}:{assr}:{res}:{fmatch}:{err}')
+                            raise err
+                else:                 
+                    # Download whole resource
+
+                    # Where shall we save it?
+                    dst = f'{sess_dir}/{assr}'
+
+                    # Have we already downloaded it?
+                    if os.path.exists(os.path.join(dst, res)):
+                        logger.debug(f'exists:{dst}')
+                        continue
+
+                    # Download it
+                    logger.debug(f'download resource:{proj}:{subj}:{sess}:{assr}:{res}')
+                    try:
+                        _download_resource(
+                            garjus,
+                            proj,
+                            subj,
+                            sess,
+                            assr,
+                            res,
+                            dst
+                        )
+                    except Exception as err:
+                        logger.error(f'{subj}:{sess}:{assr}:{res}:{err}')
+                        raise err
+
+
+def download_analysis_inputs(garjus, project, analysis_id, download_dir):
+    errors = []
+
+    print(f'download_analysis_inputs:{project}:{analysis_id}:{download_dir}')
+
+    # Make the output directory
+    _make_dirs(download_dir)
+
     # Determine what we need to download
-    a = garjus.load_analysis(project, analysis_id)
+    analysis = garjus.load_analysis(project, analysis_id)
 
-    print(a['processor'])
-
+    logging.info('loading project data')
     assessors = garjus.assessors(projects=[project])
+    scans = garjus.scans(projects=[project])
 
-    print(assessors)
+    sessions = pd.concat([
+        _sessions_from_scans(scans),
+        _sessions_from_assessors(assessors)
+    ])
+    sessions = sessions.drop_duplicates()
 
-    #'inputs': {'xnat': {'subjects': 
-    #{'sessions': {'types': 'Baseline',
-    #'assessors': [{'name': 'assr_msit', 'types': 'fmri_msit_v2',
-    #'resources': [{'resource': '1stLEVEL', 'fmatch': 'con_0002.nii.gz'},
-    #{'resource': '1stLEVEL', 'fmatch': 'behavior.txt'}]}]}}}}]
+    # Which subjects to include?
+    subjects = analysis['analysis_include'].splitlines()
 
-    # TODO: first confirm everything is found on xnat for these subjects
+    # What to download for each subject?
+    subj_spec = analysis['processor']['inputs']['xnat']['subjects']
 
-    subjects = a['analysis_include'].splitlines()
-    for s in subjects:
-        subj_assessors = assessors[assessors.SUBJECT == s]
-        print(s, subj_assessors)
+    for subj in subjects:
+        # Make the Subject download folder
+        subj_dir = f'{download_dir}/{subj}'
+        _make_dirs(subj_dir)
 
-        sdir = f'{download_dir}/{s}'
-
+        # Download the subject as specified in subj_spec
         try:
-            os.makedirs(sdir)
-        except FileExistsError:
-            pass
+            _download_subject(
+                garjus,
+                subj_dir,
+                subj_spec,
+                project,
+                subj,
+                sessions,
+                assessors)
+        except Exception as err:
+            errors.append(subj)
+            continue
 
-        # get the subjects sessions
+    # report what's missing
+    print('errors', errors)
 
-        # filter by types
-        # iterate sessions
-            # get the sessions assessors
-            # filter by resource
-
-
-    print(a['processor']['inputs']['xnat']['subjects'])
-
-
-    # Download it
-
-    # Done
+    print('done!')
