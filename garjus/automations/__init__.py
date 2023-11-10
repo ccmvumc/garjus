6500b38928678d97a6331778c892d5c7355c9c2c
bud42 <bdboyd42@gmail.com>
2023-11-10 14:33:28 -0500

    gaitrite etl

--- garjus/automations/__init__.py
+++ garjus/automations/__init__.py
@@ -204,7 +204,9 @@ def _run_etl_automation(automation, garjus, project):
         logger.debug(f'primary redcap not found:{project}')
         return
 
-    if automation == 'etl_nihexaminer':
+    if automation == 'etl_gaitrite':
+        results = _run_etl_gaitrite(project_redcap)
+    elif automation == 'etl_nihexaminer':
         results = _run_etl_nihexaminer(project_redcap)
     elif automation == 'etl_nihtoolbox_drtaylor':
         results = _run_etl_nihtoolbox_drtaylor(project_redcap)
@@ -287,6 +289,90 @@ def _run_etl_nihtoolbox_drtaylor(project):
         # Load data back to redcap
         _load(project, record_id, event_id, data)
         results.append({'subject': record_id, 'event': event_id})
+
+    return results
+
+
+def _run_etl_gaitrite(project):
+    data = {}
+    results = []
+    events = []
+    records = []
+    file_field = 'gaitrite_upload'
+    done_field = 'gaitrite_testrecord'
+
+    # load the automation
+    try:
+        mod = importlib.import_module(f'garjus.automations.etl_gaitrite')
+    except ModuleNotFoundError as err:
+        logger.error(f'error loading module:{err}')
+        return
+
+    # records will include both the file record and the repeating records
+    records = project.export_records(
+        fields=[file_field, done_field],
+        events=field2events(project, file_field)
+    )
+
+    # Get unique list of record/event
+    recordevents = list(set(
+        [(x[project.def_field], x['redcap_event_name']) for x in records]))
+
+    # Initialize counts
+    counts = {x: 0 for x in recordevents}
+
+    # Count repeats
+    repeats = [x for x in records if x['redcap_repeat_instance']]
+
+    # Count total records for each id/event
+    for r in repeats:
+        record_id = r[project.def_field]
+        event_id = r['redcap_event_name']
+        counts[(record_id, event_id)] += 1
+
+    # Process each record
+    for r in [x for x in records if not x['redcap_repeat_instance']]:
+        data = {}
+        record_id = r[project.def_field]
+        event_id = r['redcap_event_name']
+
+        if not r[file_field]:
+            logger.debug(f'no data file:{record_id}:{event_id}')
+            continue
+
+        if counts[(record_id, event_id)] > 0:
+            logger.debug(f'already done:{record_id}:{event_id}')
+            continue
+
+        logger.debug(f'etl_gaitrite:{record_id}:{event_id}')
+
+        with tempfile.TemporaryDirectory() as tmpdir:
+            data_file = f'{tmpdir}/gaitrite.xlsx'
+
+            # Download files from redcap
+            logger.debug(f'downloading file:{data_file}')
+            download_file(
+                project, record_id, file_field, data_file, event_id=event_id)
+
+            # Process downloaded file to extract data
+            data = mod.process(data_file)
+
+        # Load data back to redcap
+        results.append({'subject': record_id, 'event': event_id})
+        for d in data:
+            d[project.def_field] = record_id
+            d['redcap_event_name'] = event_id
+            d['redcap_repeat_instrument'] = 'gaitrite'
+            d['redcap_repeat_instance'] = 'new'
+            d['gaitrite_complete'] = '2'
+
+        if len(data) > 0:
+            try:
+                response = project.import_records(data)
+                assert 'count' in response
+                logger.debug(f'uploaded:{record_id}:{event_id}')
+            except AssertionError as e:
+                logger.error('error uploading', record_id, e)
 
     return results
 
--- garjus/automations/etl_gaitrite/__init__.py
+++ garjus/automations/etl_gaitrite/__init__.py
@@ -12,8 +12,31 @@ logger = logging.getLogger('garjus.automations.etl_gaitrite')
 
 def process(gaitrite_file):
     """Process Gaitrite file and return subset of data."""
+    data = []
 
-    data = _extract(gaitrite_file)
+    # Load each test
+    for d in _extract(gaitrite_file):
+        data.append({
+            'gaitrite_testrecord': d['Test Record #'],
+            'gaitrite_datetime': d['Date / Time of Test'],
+            'gaitrite_comments': d['Comments'],
+            'gaitrite_velocity': d['Velocity'],
+            'gaitrite_cadence': d['Cadence'],
+            'gaitrite_steptime_left': d['Step Time(sec) L'],
+            'gaitrite_steptime_right': d['Step Time(sec) R'],
+            'gaitrite_stepextremity_left': d['Step Extremity(ratio) L'],
+            'gaitrite_stepextremity_right': d['Step Extremity(ratio) R'],
+            'gaitrite_stridelen_left': d['Stride Length(cm) L'],
+            'gaitrite_stridelen_right': d['Stride Length(cm) R'],
+            'gaitrite_swingtime_left': d['Swing Time(sec) L'],
+            'gaitrite_swingtime_right': d['Swing Time(sec) R'],
+            'gaitrite_stancetime_left': d['Stance Time(sec)  L'],
+            'gaitrite_stancetime_right': d['Stance Time(sec)  R'],
+            'gaitrite_funcambprofile': d['Functional Amb. Profile'],
+            'gaitrite_normvelocity': d['Normalized Velocity  '],
+            'gaitrite_heeltime_left': d['Heel Off On Time L'],
+            'gaitrite_heeltime_right': d['Heel Off On Time R']
+    })
 
     return data
 
@@ -21,12 +44,18 @@ def process(gaitrite_file):
 def _extract(filename):
     """Extract data from file that has a header row and one data row"""
     try:
-        df = pd.read_csv(filename)
+        df = pd.read_csv(filename, dtype=str)
     except:
-        df = pd.read_excel(filename)
+        df = pd.read_excel(filename, dtype=str)
 
-    # Get data from last row
-    return df.to_dict()
+    df = df.dropna(subset=['Test Record #'])
+
+    df = df.sort_values('Test Record #')
+
+    # Fill nan with blanks
+    df = df.fillna('')
+
+    return df.to_records()
 
 if __name__ == "__main__":
     import pprint
@@ -41,4 +70,4 @@ if __name__ == "__main__":
 
     data = process(test_file)
     pprint.pprint(data)
-    pprint.pprint(data.keys())
+    #pprint.pprint(data.keys())
