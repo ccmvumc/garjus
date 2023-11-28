31c3c10ded7bbe1778a5cc1a47899f7e62f59aa5
bud42 <bdboyd42@gmail.com>
2023-11-28 11:14:04 -0500

    Update app.py

--- garjus/dashboard/app.py
+++ garjus/dashboard/app.py
@@ -13,7 +13,11 @@ dbc_css = "https://cdn.jsdelivr.net/gh/AnnMarieW/dash-bootstrap-templates/dbc.mi
 assets_path = os.path.expanduser('~/git/garjus/garjus/dashboard/assets')
 
 hour = datetime.datetime.now().hour
-if hour < 9 or hour > 17:
+
+darkmode = True
+
+#$if hour < 9 or hour > 17:
+if darkmode:
     stylesheets = [dbc.themes.DARKLY, dbc_css]
     load_figure_template("darkly")
 else:
