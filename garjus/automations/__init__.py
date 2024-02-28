c6081accae3f651802fd18e6ac7eefe2bb3d1f01
bud42 <bdboyd42@gmail.com>
2024-02-27 21:15:41 -0500

    Update __init__.py

--- garjus/automations/__init__.py
+++ garjus/automations/__init__.py
@@ -104,6 +104,9 @@ def _run_edat_automations(automations, garjus, project):
         logger.debug(f'primary redcap not found, check keys:{project}')
         return
 
+    # Only MRI
+    scanp = [x for x in scanp if x['scanning_modality'] == 'MRI']
+
     for p in scanp:
         s = p['scanning_xnatsuffix']
         _events = [x.strip() for x in p['scanning_events'].split(',')]
