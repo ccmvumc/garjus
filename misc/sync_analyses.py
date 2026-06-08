073ea28bffec29691ffc40210cc13cfcd8ed0826
bud42 <bdboyd42@gmail.com>
2026-06-08 11:47:09 -0400

    Update sync_analyses.py

--- misc/sync_analyses.py
+++ misc/sync_analyses.py
@@ -19,7 +19,7 @@ df = g.analyses(projects=['CHAMP', 'D3'], download=False)
 print(df.columns)
 
 
-def _check(g, a):
+def _check_report(g, a):
     if not a['OUTPUT']:
         return
 
@@ -30,34 +30,76 @@ def _check(g, a):
         print('No PDF', a['PROJECT'], a['ID'], a['OUTPUT'])
         return
 
-    print('report.pdf', a['PROJECT'], a['ID'], a['OUTPUT'])
-
     rcq = g._rcq
     rec = rcq.export_records(records=[a['PROJECT']], forms=['analyses'], fields=['analysis_reportfile'])   
     rec = [x for x in rec if x['redcap_repeat_instance'] == a['ID']]
 
     if not rec:
-        print('records not found')
+        print('records not found:', a['PROJECT'], a['ID'], a['OUTPUT'])
         return
 
     cur_report = rec[0]['analysis_reportfile']
-
     if cur_report:
-        print(f'report found:{cur_report}')
+        print(f'report found:{cur_report}', a['PROJECT'], a['ID'], a['OUTPUT'])
         return
 
-    print('no report, copy')
+    print('no report, copying:', a['PROJECT'], a['ID'], a['OUTPUT'])
     with tempfile.TemporaryDirectory() as tmpdir:
-        # download file from xnati
+        # download file from xnat
         dst = f'{tmpdir}/report.pdf'
-        print('download from xnat', dst)
+        print('DOWNLOAD from xnat', dst)
         xnat_res_file.get(dst)
 
         # upload file to redcap
-        print('upload to redcap', dst)
+        print('UPLOAD to redcap', dst)
         upload_file(rcq, a['PROJECT'], 'analysis_reportfile', dst, repeat_id=[a['ID']])
 
 
-for i, a in df.iterrows():
-    _check(g, a)
+def _check_log(g, a):
+    file_field = 'analysis_logfile'
 
+    if not a['OUTPUT']:
+        return
+
+    filename = f'{a["OUTPUT"]}.txt'
+
+    res = g.xnat().select_project(a['PROJECT']).resource(a['OUTPUT'])
+
+    xnat_res_file = res.file(filename)
+
+    if not xnat_res_file.exists():
+        print(f'{filename}:file not on XNAT', a['PROJECT'], a['ID'], a['OUTPUT'])
+        return
+
+    rcq = g._rcq
+    rec = rcq.export_records(records=[a['PROJECT']], forms=['analyses'], fields=[file_field])   
+    rec = [x for x in rec if x['redcap_repeat_instance'] == a['ID']]
+
+    if not rec:
+        print('records not found:', a['PROJECT'], a['ID'], a['OUTPUT'])
+        return
+
+    cur_file = rec[0][file_field]
+    if cur_file:
+        print(f'{file_field}:found:{cur_file}', a['PROJECT'], a['ID'], a['OUTPUT'])
+        return
+
+    print(f'no {file_field}, copying:', a['PROJECT'], a['ID'], a['OUTPUT'])
+    with tempfile.TemporaryDirectory() as tmpdir:
+        # download file from xnat
+        dst = f'{tmpdir}/log.txt'
+        print('DOWNLOAD from xnat', dst)
+        xnat_res_file.get(dst)
+
+        # upload file to redcap
+        print('UPLOAD to redcap', dst)
+        upload_file(rcq, a['PROJECT'], file_field, dst, repeat_id=[a['ID']])
+
+
+
+
+for i, a in df.iterrows():
+    #_check_report(g, a)
+    _check_log(g, a)
+    #_check_batch(g, a)
+    #_check_stats(g, a)
